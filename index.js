aa8de72b8f2a0a94cf33f16139869f1da94d6f0c
ShineShao <xiaoshaoqq@gmail.com>
2024-01-20 23:21:59 +0800

    vesion: 1.7.0-alpha.0

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: true,
   },
   plugins: ['react', 'jsx-a11y', 'react-hooks', '@typescript-eslint'],
   rules: {

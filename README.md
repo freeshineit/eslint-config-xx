6ad95a4e8122c680d36a71f75487d5697484967e
ShineShao <xiaoshaoqq@gmail.com>
2025-08-20 23:48:17 +0800

    feat: update eslint@9

--- README.md
+++ README.md
@@ -5,14 +5,17 @@
 ![License](https://img.shields.io/npm/l/eslint-config-xx.svg)
 
 Support js/ts/jsx/tsx and export prettier config
+
 ## install
 
 ```bash
 yarn install eslint-config-xx -D
 ```
+
 ### eslint config
 
 `.eslintrc.js`
+
 ```js
 module.exports = {
   extends: 'xx',
@@ -33,47 +36,47 @@ module.exports = {
     // '@typescript-eslint/no-unused-vars': 1, // warn
     // 'react-hooks/exhaustive-deps': 2 // error
   },
-}
-
+};
 ```
 
 ### prettier config
 
 `.prettierrc.js`
+
 ```js
 module.exports = {
   ...require('eslint-config-xx/prettier.config'),
-}
+};
 ```
-
 
 ### js 项目
 
-Parsing error: project was set to `true` but couldn't find any tsconfig.json relative 
+Parsing error: project was set to `true` but couldn't find any tsconfig.json relative
 
 在根目录下新建`tsconfig.json`
+
 ```json
 {
-    "compilerOptions": {
-        "module": "esnext",
-        "target": "ES2015",
-        "lib": ["es6", "dom", "es2016", "es2017"],
-        "sourceMap": true,
-        "allowJs": true,
-        "outDir": "dist",
-        "moduleResolution": "node",
-        "forceConsistentCasingInFileNames": true,
-        "noImplicitReturns": true,
-        "noImplicitThis": true,
-        "noImplicitAny": true,
-        "strictNullChecks": true,
-        "noUnusedLocals": true,
-        "noUnusedParameters": true,
-        "esModuleInterop": true,
-        "resolveJsonModule": true,
-        "jsx": "react-jsx"
-    },
-    "include": ["./index.js", "./.prettierrc.cjs"],
-    "exclude": ["node_modules"]
+  "compilerOptions": {
+    "module": "esnext",
+    "target": "ES2015",
+    "lib": ["es6", "dom", "es2016", "es2017"],
+    "sourceMap": true,
+    "allowJs": true,
+    "outDir": "dist",
+    "moduleResolution": "node",
+    "forceConsistentCasingInFileNames": true,
+    "noImplicitReturns": true,
+    "noImplicitThis": true,
+    "noImplicitAny": true,
+    "strictNullChecks": true,
+    "noUnusedLocals": true,
+    "noUnusedParameters": true,
+    "esModuleInterop": true,
+    "resolveJsonModule": true,
+    "jsx": "react-jsx"
+  },
+  "include": ["./index.js", "./.prettierrc.cjs"],
+  "exclude": ["node_modules"]
 }
 ```
---
+++ eslint.config.mjs
@@ -0,0 +1,117 @@
+// eslint.config.js
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import { configs } from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
+
+// 合并所有配置
+export default [
+  // 基础配置
+  js.configs.recommended,
+  ...configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+
+  // React 配置
+  {
+    files: ['**/*.{js,mjs,jsx,ts,tsx}'],
+    ignores: ['**/dist/**', '**/build/**'],
+
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      prettier: pluginPrettier,
+    },
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2022,
+      },
+    },
+
+    rules: {
+      // ========== React 规则 ==========
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react/jsx-boolean-value': ['error', 'never'],
+      'react/jsx-closing-bracket-location': 'error',
+      'react/jsx-closing-tag-location': 'error',
+      'react/jsx-curly-spacing': ['error', { when: 'never', children: true }],
+      'react/jsx-equals-spacing': ['error', 'never'],
+      'react/jsx-first-prop-new-line': ['error', 'multiline'],
+      'react/jsx-fragments': ['error', 'syntax'],
+      'react/jsx-no-useless-fragment': 'error',
+      'react/jsx-pascal-case': 'error',
+      'react/jsx-props-no-multi-spaces': 'error',
+      'react/jsx-tag-spacing': 'error',
+      'react/no-children-prop': 'error',
+      'react/no-danger-with-children': 'error',
+
+      // ========== React Hooks 规则 ==========
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // ========== TypeScript 规则 ==========
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+
+      // ========== 基础 ESLint 规则 ==========
+      'no-unused-vars': 'off', // 使用 TypeScript 的版本
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prefer-const': 'error',
+      'no-var': 'error',
+      // Prettier
+      'prettier/prettier': 'error',
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json', // Adjust path if your tsconfig.json is elsewhere
+        },
+      },
+    },
+  },
+
+  // 禁用所有与 Prettier 冲突的规则
+  {
+    files: ['**/*.{js,mjs,jsx,ts,tsx}'],
+    rules: {
+      ...prettier.rules,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json', // Adjust path if your tsconfig.json is elsewhere
+        },
+      },
+    },
+  },
+];
